dd5a030c22f9a031c306ba4aff3afad820683f1c
Muhammad Mohsin Saleem <hafizmohsinsaleem786@gmail.com>
2025-03-12 21:57:41 +0500

    Initial commit

---
+++ lab1.py
@@ -0,0 +1,47 @@
+#my first python statement
+print("Hello, World")
+
+#input statements
+# txt = input()
+#OR
+# txt = input("Type a number, letter or special symbol")
+# print(txt)
+
+#multiple statements on a single line
+print("Mohsin")
+print("Saleem")
+#OR
+print("Mohsin");print("Saleem")
+
+#use indentation to form a block of code
+#if x>0:
+#print("This statement will throw an error due to no indentation")
+
+x=1
+if x>0:
+ print("Single Space indentation")
+
+x=1
+if x>0:
+	print("Single tab indentation")
+
+x=1
+if x>0:
+    print("use 4 spaces per indentation and no tabs")
+
+#checking type of variable
+a=4.5
+print (type(a))
+
+str1 = "Safe"
+print(str1)
+
+list1 = [2,4,6,8]
+print(list1) 
+
+list2 = ['Mohsin','BCS22006',3.98]
+print(list2)
+
+print(list2[0],list2[2])
+
+print(list2[0:2])
---
+++ lab2.py
@@ -0,0 +1,19 @@
+c = 0
+while (c<2):
+    print("You are my fvt")
+    c = c+1
+
+print("Printing the list using for in loop")
+list = ['Mohsin','CR',3.98,"He is a good boy"]
+for i in list:
+    print(i)
+
+print("\nTuple iteration")
+list1 = ("I am very cute","Dont be jealous yr","You are my desh")
+for i in list1:
+    print(i)
+
+print("\nString iteration")
+s = "GoodDay"
+for i in s:
+    print(i)
